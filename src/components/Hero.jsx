e6ddcf0516d8ccd33de4b5f4f05cff350f0529f6
Ryan Mangum <ryanmngm@gmail.com>
2026-08-07 16:38:46 -0600

    Update MNGM Group styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
+import logo from "../assets/mngm-logo.png";
+
 function Hero() {
   return (
     <section className="hero">
+      <h1 className="hero-company-name">MNGM Group</h1>
+
+      <img
+        src={logo}
+        alt="MNGM Group"
+        className="hero-logo"
+      />
+
       <h1>MNGM</h1>
 
       <p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,5 @@
-import logo from "../assets/mngm-logo.png";
-
 function Navbar() {
-  return (
-    <nav className="navbar">
-      <img
-        src={logo}
-        alt="MNGM Group"
-        className="navbar-logo"
-        width="60"
-        height="60"
-      />
-
-      <h1 className="navbar-title">
-        MNGM Group
-      </h1>
-    </nav>
-  );
+  return null;
 }
 
 export default Navbar;
