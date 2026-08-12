a362e0412d871b136d6168e64af5686cb41fd1b5
Ryan Mangum <ryanmngm@gmail.com>
2026-08-11 21:51:59 -0600

    Fix external links

--- src/data/companies.js
+++ src/data/companies.js
@@ -24,7 +24,7 @@ const companies = [
     title: "MNGM Studios",
     description: "Creative & Software Studio",
     status: "green",
-    link: "mngmstudios.com",
+    link: "https://www.mngmstudios.com",
   },
 
   {
